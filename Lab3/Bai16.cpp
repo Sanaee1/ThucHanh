8446be8716fcf3832c932aff809404df0bf5ad55
Iris101218 <ducddh2310058@student.hiu.vn>
2024-01-02 16:37:06 +0700

    nah

--- Lab3/Bai16.cpp
+++ Lab3/Bai16.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-void tong(int array[3][4]) {
+void tong(int array[30][30], int m, int n) {
     int sum = 0;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < m; i++) {
         sum = 0;
-        for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < n; j++) {
             sum += array[i][j];
         }
         cout << "Tong dong " << i + 1 << " la: " << sum << endl;
@@ -14,18 +14,25 @@ void tong(int array[3][4]) {
 }
 
 int main() {
-    int array[3][4];
+    int m;
+    int n;
+    int array[30][30];
+
+    cout << "Nhap so dong: ";
+    cin >> m;
+    cout << "Nhap so cot: ";
+    cin >> n;
 
     cout << "Nhap cac phan tu cua mang: " << endl;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < m; i++) {
         cout << "Nhap cac phan tu cua dong " << i + 1 << ": " << endl;
-        for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < n; j++) {
             cout << "Nhap phan tu thu " << j + 1 << ": ";
             cin >> array[i][j];
         }
     }
 
-    tong(array);
+    tong(array, m, n);
 
     system("pause");
     return 0;
