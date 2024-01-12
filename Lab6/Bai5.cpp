22a8e37476b92a1031739c76b6ddb2c7bdea0e71
Iris101218 <ducddh2310058@student.hiu.vn>
2024-01-12 09:07:26 +0700

    ...

--- Lab6/Bai5.cpp
+++ Lab6/Bai5.cpp
@@ -1,57 +1,143 @@
 #include <iostream>
 using namespace std;
 
-void menu(int n, int matrix[][]) {
-menu:
-    char opt;
-    cout << "a. Nhap ma tran" << endl;
-
-    switch (opt) {
-        case 'a':
-        matrix:
-            int opt;
-            cout << "1. Nhap ma tran bang tay" << endl;
-            cout << "2. Nhap ma tran ngau nhien" << endl;
-            cout << "3. Quay lai" << endl;
-            cout << "Nhap lua chon: ";
-            cin >> opt;
-            switch (opt) {
-                case 1:
-                    cout << "Nhap ma tran: " << endl;
-                    for (int i = 0; i < n; i++) {
-                        for (int j = 0; j < n; j++) {
-                            cout << "Nhap phan tu [" << i << "][" << j << "]: ";
-                            cin >> matrix[i][j];
-                        }
-                    }
-                    break;
-                case 2:
-                    cout << "Nhap ma tran: " << endl;
-                    srand(time(NULL));
-                    for (int i = 0; i < n; i++) {
-                        for (int j = 0; j < n; j++) {
-                            matrix[i][j] = rand() % 100;
-                        }
-                    }
-                    break;
-                case 3:
-                    goto menu;
-                    break;
-                default:
-                    cout << "Lua chon khong hop le!" << endl;
-                    goto matrix;
+void Sapxeptheodong(int n, int matran[50][50]) {
+    int i, j, k, temp;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - 1; j++) {
+            for (k = j + 1; k < n; k++) {
+                if (matran[i][j] > matran[i][k]) {
+                    temp = matran[i][j];
+                    matran[i][j] = matran[i][k];
+                    matran[i][k] = temp;
+                }
             }
+        }
     }
 }
 
+void Sapxeptheocot(int n, int matran[50][50]) {
+    int i, j, k, temp;
+    for (j = 0; j < n; j++) {
+        for (i = 0; i < n - 1; i++) {
+            for (k = i + 1; k < n; k++) {
+                if (matran[i][j] > matran[k][j]) {
+                    temp = matran[i][j];
+                    matran[i][j] = matran[k][j];
+                    matran[k][j] = temp;
+                }
+            }
+        }
+    }
+}
+
+void Sapxeptheocheochinh(int n, int matran[50][50]) {
+    int i, j, k, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (matran[i][i] > matran[j][j]) {
+                temp = matran[i][i];
+                matran[i][i] = matran[j][j];
+                matran[j][j] = temp;
+            }
+        }
+    }
+}
+
+void Sapxeptheocheophu(int n, int matran[50][50]) {
+    int i, j, k, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (matran[i][n - i - 1] > matran[j][n - j - 1]) {
+                temp = matran[i][n - i - 1];
+                matran[i][n - i - 1] = matran[j][n - j - 1];
+                matran[j][n - j - 1] = temp;
+            }
+        }
+    }
+}
+
+void menu(int n, int matran[50][50]) {
+menu:
+    char opt;
+    cout << "a. Xuat ma tran\n";
+    cout << "b. Sap xep tang dan cac gia tri trong tung dong\n";
+    cout << "c. Sap xep tang dan cac gia tri trong tung cot\n";
+    cout << "d. Sap xep tang dan cac gia tri nam tren duong cheo chinh\n";
+    cout << "e. Sap xep tang dan cac gia tri nam tren duong cheo phu\n";
+    cout << "f. Thoat.\n";
+    cout << "Nhap lua chon: ";
+    cin >> opt;
+    switch (opt) {
+        case 'a':
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    cout << matran[i][j] << " ";
+                }
+                cout << endl;
+            }
+            goto menu;
+        case 'b':
+            Sapxeptheodong(n, matran);
+            goto menu;
+        case 'c':
+            Sapxeptheocot(n, matran);
+            goto menu;
+        case 'd':
+            Sapxeptheocheochinh(n, matran);
+            goto menu;
+        case 'e':
+            Sapxeptheocheophu(n, matran);
+            goto menu;
+        case 'f':
+            break;
+        default:
+            cout << "Lua chon khong hop le!\n";
+            goto menu;
+    }
+}
+
+
 int main() {
     int n;
-    int matrix[10][10];
     cout << "Nhap n: ";
     cin >> n;
+    int matran[50][50];
 
-    menu(n, matrix);
+matrix:
+    int o;
+    cout << "1. Nhap tu ban phim \n";
+    cout << "2. Ngau nhien\n";
+    cout << "0. Thoat.\n";
+    cout << "Nhap lua chon: ";
+    cin >> o;
+    int i,j;
+    switch (o) {
+        case 1:
+            //Nhập tay
+            for (i = 0; i < n; i++) {
+                cout << "Nhap dong " << i + 1 << ": ";
+                for (j = 0; j < n; j++) {
+                    cin >> matran[i][j];
+                    }
+                }
+            break;
+        case 2:
+            //Ngẫu nhiên ma trận
+            for (i = 0; i < n; i++) {
+                for (j = 0; j < n; j++) {
+                    matran[i][j] = rand() % 100;
+                }
+            }
+            break;
+        case 0:
+            break;
+        default:
+            cout << "Lua chon khong hop le!\n";
+            goto matrix;
+    }
 
-    system("pause");
+    menu(n, matran);
+
     return 0;
 }
